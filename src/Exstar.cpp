bf56d87291c77d6e2bbc066c50dd160cc256c4f6
benajan09@gmail.com <benajan09@gmail.com>
2021-12-09 14:29:58 -0600

    fixed indentation

--- src/Exstar.cpp
+++ src/Exstar.cpp
@@ -414,71 +414,62 @@ void exstar::Camera::drawSprite(exstar::Sprite* sprite,int x,int y){
 	glDeleteShader(verexShader);
 	glDeleteShader(fragmentShader);
 	//define params
-	float vertices[] = {
-        // positions          // colors           // texture coords
-         1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-         1.0f, 0.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-         0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-         0.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
-     };
-     unsigned int indices[] = {  
-        0, 1, 3, // first triangle
-        1, 2, 3  // second triangle
-    };
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
+	float vertices[] = {1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, 1.0f, 0.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, 0.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f };
+	unsigned int indices[] = {  0, 1, 3,1, 2, 3};
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
-    glBindVertexArray(VAO);
+	glBindVertexArray(VAO);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
     // texture coord attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-    unsigned int texture;
-    glGenTextures(1,&texture);
-    glBindTexture(GL_TEXTURE_2D,texture);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+	unsigned int texture;
+	glGenTextures(1,&texture);
+	glBindTexture(GL_TEXTURE_2D,texture);
     // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     //Load image according to its type(RGB,RGBA)
-    if(sprite->getType() == 3){
-    	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, sprite->getTextureWidth(), sprite->getTextureHeight(), 0, GL_RGB, GL_UNSIGNED_BYTE, sprite->getImage());
-    	glGenerateMipmap(GL_TEXTURE_2D);
-    }else{
-    	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, sprite->getTextureWidth(), sprite->getTextureHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, sprite->getImage());
-    	glGenerateMipmap(GL_TEXTURE_2D);
-    }
+	if(sprite->getType() == 3){
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, sprite->getTextureWidth(), sprite->getTextureHeight(), 0, GL_RGB, GL_UNSIGNED_BYTE, sprite->getImage());
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}else{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, sprite->getTextureWidth(), sprite->getTextureHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, sprite->getImage());
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
     //Transformations
-    glm::mat4 projection;
-    projection = glm::ortho(pos->x+0.0f,(float)size->width+pos->x,(float)size->height+pos->y,0.0f+pos->y,-1.0f,1.0f);
-    glm::mat4 ModelMatrix(1.0f);
-    ModelMatrix = glm::translate(ModelMatrix,glm::vec3(x,y,0.0f));
-    ModelMatrix = glm::scale(ModelMatrix,glm::vec3(sprite->getWidth(),sprite->getHeight(),1.0f));
+	glm::mat4 projection;
+	projection = glm::ortho(pos->x+0.0f,(float)size->width+pos->x,(float)size->height+pos->y,0.0f+pos->y,-1.0f,1.0f);
+	glm::mat4 ModelMatrix(1.0f);
+	ModelMatrix = glm::translate(ModelMatrix,glm::vec3(x,y,0.0f));
+	ModelMatrix = glm::scale(ModelMatrix,glm::vec3(sprite->getWidth(),sprite->getHeight(),1.0f));
 	//Draw image and cleanup
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glUseProgram(shaderProgram);
-    glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"ModelMatrix"),1,GL_FALSE,glm::value_ptr(ModelMatrix));
-    glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"projection"),1,GL_FALSE,glm::value_ptr(projection));
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
-    glBindVertexArray(0);
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glUseProgram(shaderProgram);
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"ModelMatrix"),1,GL_FALSE,glm::value_ptr(ModelMatrix));
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgram,"projection"),1,GL_FALSE,glm::value_ptr(projection));
+	glBindVertexArray(VAO);
+	glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+	glBindVertexArray(0);
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 }
 void exstar::Camera::drawSprite(exstar::Sprite* sprite,exstar::Point pos){
 	drawSprite(sprite,pos.x,pos.y);
