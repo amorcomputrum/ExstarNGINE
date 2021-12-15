6753103896674ab373e80c5ae33884a3efd3171e
benajan09@gmail.com <benajan09@gmail.com>
2021-12-14 18:51:29 -0600

    Implemented drawEllipse

--- test/include/Window.hpp
+++ test/include/Window.hpp
@@ -4,9 +4,10 @@ class Window : public exstar::Window
 public:
 	Window(int width,int height,const char* title) : exstar::Window(width,height,title) {
 		setFramerate(60);
-
 	}
 	void render(exstar::Camera camera){
+		camera.setColor(exstar::Color::Blue);
+		camera.drawEllipse(100,100,50,100);
 	}
 
 };
