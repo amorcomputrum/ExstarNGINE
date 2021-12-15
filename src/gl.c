40abd42c0e221e99e09f3d6775c707c399c27b38
benajan09@gmail.com <benajan09@gmail.com>
2021-12-15 09:50:17 -0600

    Sorted includes

--- src/gl.c
+++ src/gl.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <Exstar/exstarglad/gl.h>
+#include "Exstar/exstarglad/gl.h"
 
 #ifndef GLAD_IMPL_UTIL_C_
 #define GLAD_IMPL_UTIL_C_
