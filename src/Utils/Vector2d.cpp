eaf303cab9b9054ef9aaf88ebd17d6fae4f65017
benajan09@gmail.com <benajan09@gmail.com>
2021-12-20 16:24:16 -0600

    Added JavaDoc Format Documentation and normalize()

--- src/Utils/Vector2d.cpp
+++ src/Utils/Vector2d.cpp
@@ -45,3 +45,6 @@ exstar::Vector2d exstar::Vector2d::operator*(double param){
 float exstar::Vector2d::dot(exstar::Vector2d vec1,exstar::Vector2d vec2){
 	return (vec1.x*vec2.x) + (vec2.y * vec1.y);
 }
+exstar::Vector2d exstar::Vector2d::normalize(exstar::Vector2d vec){
+	return exstar::Vector2d(vec.x/abs(vec.magnitude()),vec.y/abs(vec.magnitude()));
+}
