5d7d5abba04a7c124598bc589acd71b12677712d
benajan09@gmail.com <benajan09@gmail.com>
2021-12-20 16:16:06 -0600

    Added JavaDoc Format Documentation

--- src/Utils/Vector2d.cpp
+++ src/Utils/Vector2d.cpp
@@ -46,5 +46,5 @@ exstar::Vector2d exstar::Vector2d::operator*(double param){
 
 
 float exstar::Vector2d::dot(exstar::Vector2d vec1,exstar::Vector2d vec2){
-	return (vec1.x*vec2.y) + (vec2.x * vec1.y);
+	return (vec1.x*vec2.x) + (vec2.y * vec1.y);
 }
