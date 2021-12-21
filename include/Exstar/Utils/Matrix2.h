4596abd5fd0f51e2bcb594e1c365dc0ee21fcbce
benajan09@gmail.com <benajan09@gmail.com>
2021-12-21 12:30:03 -0600

    Implemented 2x2 Matrix

---
+++ include/Exstar/Utils/Matrix2.h
@@ -0,0 +1,32 @@
+#include "Exstar/Utils/Vector2d.h"
+namespace exstar{
+	class Matrix2{
+	public:
+		float m00,m01,m10,m11;
+		Matrix2(float m00,float m01,float m10,float m11);
+		/**
+		 * Create from angle in radian
+		 * 
+		 * @param radians The radian of the angle
+		*/
+		void set(float radians);
+		/**
+		 * Transpose matrix
+		 * 
+		 * @return transposed matrix
+		*/
+		exstar::Matrix2 transpose();
+		/**
+		 * Mulitply 2x2 Matrix by 2x1
+		 * 
+		 * @param param 2x1 to multiply by
+		*/
+		exstar::Vector2d operator*(exstar::Vector2d param);
+		/**
+		 * Mulitply 2x2 Matrix by 2x2
+		 * 
+		 * @param param 2x2 to multiply by
+		*/
+		exstar::Matrix2 operator*(exstar::Matrix2 param);
+	}
+}
