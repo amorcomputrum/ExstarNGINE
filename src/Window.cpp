9fc7fe44a45d7319467f997041bb8be9dc3b5516
benajan09@gmail.com <benajan09@gmail.com>
2022-01-04 15:47:51 -0600

    added MSAA 4

--- src/Window.cpp
+++ src/Window.cpp
@@ -27,8 +27,11 @@ void exstar::Window::run(){
 	glfwSwapInterval(1);
 	//allow Forward compatability with opengl
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glEnable(GL_MULTISAMPLE); 
 	//create clock
-	clock = new exstar::Clock();
+	clock = new exstar::Clock(); 
 	//begin loop
 	update();
 	//close when loop closes
