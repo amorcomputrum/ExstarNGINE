4552a2214b87574fdd1c0ff5581f9362aa003d90
benajan09@gmail.com <benajan09@gmail.com>
2022-02-07 20:09:56 -0600

    Made some Texture Wrapping and Filtering adjustments

--- src/Graphics/Sprite/Sprite.cpp
+++ src/Graphics/Sprite/Sprite.cpp
@@ -107,10 +107,10 @@ void exstar::Sprite::loadShader(){
 	glBindTexture(GL_TEXTURE_2D, texture);
 
     // set texture filtering parameters
-	//glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	//glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_LINEAR              );
-	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
-	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); 
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER, GL_LINEAR     );
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_LINEAR     );
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );	
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE ); 
 
     //Load Sprite according to its type(RGB,RGBA)
 	if(type == 3){
